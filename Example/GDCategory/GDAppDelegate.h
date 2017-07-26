e9fb4dfccb84726cbb1094d9cedf7d6c097d9608
binins <gddabinbin@gmail.com>
2017-07-26 17:25:46 +0800

    Initial commit

---
+++ Example/GDCategory/GDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GDAppDelegate.h
+//  GDCategory
+//
+//  Created by binbins on 07/26/2017.
+//  Copyright (c) 2017 binbins. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GDCategory/GDViewController.h
@@ -0,0 +1,13 @@
+//
+//  GDViewController.h
+//  GDCategory
+//
+//  Created by binbins on 07/26/2017.
+//  Copyright (c) 2017 binbins. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GDViewController : UIViewController
+
+@end
